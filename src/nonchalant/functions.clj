122b05b739037a742f8f964d0ed6f753b0f308d4
Nonchalantcode <hectorg269s@outlook.com>
2020-09-11 12:44:05 -0600

    some refactoring done. Detecting content type

--- src/nonchalant/functions.clj
+++ src/nonchalant/functions.clj
@@ -7,18 +7,26 @@
 
 (defn noop [])
 
-(defn- open-stream ^BufferedInputStream [^String url headers]
+(defn- open-connection ^java.net.URLConnection [^String url]
+  (.openConnection (URL. url)))
+
+(defn- open-stream ^BufferedInputStream [^java.net.URLConnection connection headers]
   (try 
-    (let [connection (-> url (URL.) (.openConnection))]
-      (if (nil? headers)
-        (noop)
-        (doseq [[header header-value] headers]
-          (.setRequestProperty connection header header-value)))
-      (.connect connection)
-      (-> connection (.getInputStream) (BufferedInputStream.)))
+    (if (nil? headers)
+      (noop)
+      (doseq [[header header-value] headers]
+        (.setRequestProperty connection header header-value)))
+    (.connect connection)
+    (-> connection (.getInputStream) (BufferedInputStream.))
     (catch Exception err
       (println (.getMessage err)))))
-      
+
+(defn file-extension [^java.net.URLConnection connection]
+  (let [ext (.getContentType connection)]
+    (if (nil? ext)
+      nil
+      (str "." (second (.split ext "\\/"))))))
+
 (defn- read-stream [^java.io.BufferedInputStream stream size]
   (repeatedly (fn []
                 (let [barr (byte-array size)]
@@ -30,8 +38,9 @@
                             headers nil
                             size 1024
                             filename (str (gensym))}}]
-  (with-open [stream (open-stream url headers)
-              file (FileOutputStream. (File. ^File dest ^String filename))]
-    (doseq [chunk (take-while (fn [[bytes-read]] (pos? bytes-read))
-                              (read-stream stream size))]
-      (.write ^FileOutputStream file (second chunk) 0 (first chunk)))))
+  (let [connection (open-connection url)]
+    (with-open [stream (open-stream connection headers)
+                file (FileOutputStream. (File. ^File dest (str filename (file-extension connection))))]
+      (doseq [chunk (take-while (fn [[bytes-read]] (pos? bytes-read))
+                                (read-stream stream size))]
+        (.write ^FileOutputStream file (second chunk) 0 (first chunk))))))
